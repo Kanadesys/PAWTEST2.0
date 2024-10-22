16ac4ef126cd9dc6e2f2ef35dcb4fc5ac8bfeb08
20040815 <ovankurniawan04@gmail.com>
2024-10-22 10:28:27 +0700

    w
    w

--- PAW2/app.js
+++ PAW2/app.js
@@ -28,24 +28,24 @@ app.use((req,res,next)=>{
 });
 
 
+ mongoose.connect(
+   "mongodb+srv://Kanadesys:<db_password>@cluster0.9uc9n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ ).then(()=>{
+   console.log("Connected to Database");
+ }).catch((err)=>{
+   console.error('App starting error:', err.stack);
+   console.log("Connection Failed");
+ });
+
+
 // mongoose.connect(
-//   "mongodb+srv://mdp:peSlThYtcsmwgvV8@cluster0.n214x.mongodb.net/dbbuku?retryWrites=true&w=majority&appName=Cluster0"
+//   "mongodb://localhost:27017/dbbuku"
 // ).then(()=>{
 //   console.log("Connected to Database");
 // }).catch((err)=>{
-//   console.error('App starting error:', err.stack);
+//   // console.error('App starting error:', err.stack);
 //   console.log("Connection Failed");
 // });
-
-
-mongoose.connect(
-  "mongodb://localhost:27017/dbbuku"
-).then(()=>{
-  console.log("Connected to Database");
-}).catch((err)=>{
-  // console.error('App starting error:', err.stack);
-  console.log("Connection Failed");
-});
 
 
 // view engine setup
